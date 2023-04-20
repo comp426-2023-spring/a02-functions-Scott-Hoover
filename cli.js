3104cdea7aade21c4fca8d94fdb41c08e6acc145
Scott-Hoover <sc0tt@email.unc.edu>
2023-04-20 17:42:36 -0400

    Good mood coding

---
+++ cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist'
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+
+
+const args = minimist(process.argv.slice(2))
+
+if(args.h){
+
+    let help_ = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`
+  
+    process.stdout.write(help_)
+    process.exit()
+}
+
+const timezone = args.z || moment.tz.guess();
+const days = args.d
+
+var latitude = args.n || args.s * -1;
+var longitude = args.e || args.w * -1;
+
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&timezone='+timezone+'&daily=precipitation_hours&current_weather=true');
+
+const data = await response.json();
+
+if (args.j){
+    console.log(data);
+    process.exit(0);
+}
+var day;
+if (args.d == null){
+    day = 1;
+} else {
+    day = args.d
+}
+let curr = ""
+if (day > 1){
+    curr = 'in '+day+' days.\n'
+} else if (day == 1){
+    curr = 'tomorrow.\n'
+} else {
+    curr = 'today.\n'
+}
+
+process.exit(0);
